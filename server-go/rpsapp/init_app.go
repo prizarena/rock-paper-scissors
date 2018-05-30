a8a8c1eb9d54cc272a417d501986a052a5fadb58
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2018-05-30 22:13:30 +0100

    Fixed imports

--- server-go/rpsapp/init_app.go
+++ server-go/rpsapp/init_app.go
@@ -3,7 +3,7 @@ package rpsapp
 import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/strongo/bots-framework/core"
-	"github.com/strongo-games/rock-paper-scissors/server-go/rspbot"
+	"github.com/strongo-games/rock-paper-scissors/server-go/rpsbot"
 )
 
 func InitApp(httpRouter *httprouter.Router, botHost bots.BotHost) {
